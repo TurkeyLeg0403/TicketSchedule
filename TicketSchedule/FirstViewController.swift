4ba2d5d02ec290bba8e4bc024de5f382143abdd4
Takaki Otsu <mosaotsutakaki@gmail.com>
2019-03-24 22:53:11 +0900

    Initial Commit

---
+++ TicketSchedule/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  TicketSchedule
+//
+//  Created by Takaki Otsu on 2019/03/24.
+//  Copyright © 2019 Takaki Otsu. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
