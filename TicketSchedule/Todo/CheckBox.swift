d003bbd31a494d9a17920c18b799c84904beb2c1
Takaki Otsu <turkeyleg0403@gmail.com>
2019-03-26 22:59:47 +0900

    add checkmarker in todocell

--- TicketSchedule/Todo/CheckBox.swift
+++ TicketSchedule/Todo/CheckBox.swift
@@ -8,17 +8,18 @@ class CheckBox: UIButton {
     // Bool property
     var isChecked: Bool = false {
         didSet{
-            if isChecked == true {
-                self.setImage(checkedImage, for: UIControl.State.normal)
-            } else {
+            if isChecked {
                 self.setImage(uncheckedImage, for: UIControl.State.normal)
+            } else {
+                self.setImage(checkedImage, for: UIControl.State.normal)
             }
         }
     }
     
     override func awakeFromNib() {
         self.addTarget(self, action:#selector(buttonClicked(sender:)), for: UIControl.Event.touchUpInside)
-        self.isChecked = false
+        self.isChecked = true
+        
     }
     
     @objc func buttonClicked(sender: UIButton) {
--- TicketSchedule/Todo/TodoTableCell.swift
+++ TicketSchedule/Todo/TodoTableCell.swift
@@ -11,16 +11,21 @@ import UIKit
 class TodoTableCell: UITableViewCell {
 
     @IBOutlet weak var content: UILabel!
+    @IBOutlet weak var checkBox: CheckBox!
+    
+    var isChecked = false
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
     
+    @IBAction func checkBox(_ sender: CheckBox!) {
+//        print("checkBox isChecked:  \(sender.isChecked)")
+        // get isChecked from Btn instance
+        self.isChecked = sender.isChecked
+    }
 }
