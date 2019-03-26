9bf655000741fcd280ae3f7f69de3b00a028f346
Takaki Otsu <turkeyleg0403@gmail.com>
2019-03-26 16:59:52 +0900

    add setting icon

--- TicketSchedule/Setting/SettingVC.swift
+++ TicketSchedule/Setting/SettingVC.swift
@@ -1,11 +1,3 @@
-//
-//  SettingVC.swift
-//  TicketSchedule
-//
-//  Created by Takaki Otsu on 2019/03/26.
-//  Copyright © 2019 Takaki Otsu. All rights reserved.
-//
-
 import UIKit
 
 class SettingVC: UIViewController {
@@ -13,8 +5,6 @@ class SettingVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-    
 
 }
---
+++ TicketSchedule/Setting/SettingView.swift
@@ -0,0 +1,25 @@
+//
+//  SettingVIew.swift
+//  TicketSchedule
+//
+//  Created by Takaki Otsu on 2019/03/26.
+//  Copyright © 2019 Takaki Otsu. All rights reserved.
+//
+
+import UIKit
+
+class SettingView: UIView {
+
+    // コードから生成した時の初期化処理
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.loadNib()
+    }
+    
+    // ストーリーボードで配置した時の初期化処理
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.loadNib()
+    }
+
+}
